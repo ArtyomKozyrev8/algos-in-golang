61901576c704f9bb151689243653ddfaeda4b8c8
avkozyrev <avkozyrev@domclick.ru>
2025-11-09 20:24:30 +0300

    new stuct OrderedAscLinkedList

---
+++ ordered_linked_list.go
@@ -0,0 +1,53 @@
+package algos_in_golang
+
+import "fmt"
+
+type OrderedAscLinkedList struct {
+	head *Node
+}
+
+func (list *OrderedAscLinkedList) PrintList() string {
+	res := "OAscLL: "
+	cur := list.head
+	for {
+		if cur != nil {
+			res += fmt.Sprintf("%d", cur.value)
+			cur = cur.next
+			if cur != nil {
+				res += ","
+			}
+		} else {
+			break
+		}
+	}
+
+	return res
+}
+
+func (list *OrderedAscLinkedList) Append(value int) {
+	if list.head == nil {
+		list.head = &Node{value: value}
+		return
+	}
+
+	var prev *Node
+	cur := list.head
+	for {
+		if cur.value <= value {
+			prev = cur
+			cur = cur.next
+			if cur == nil {
+				prev.next = &Node{value: value}
+				break
+			}
+		} else {
+			if prev == nil {
+				list.head = &Node{value: value, next: cur}
+			} else {
+				prev.next = &Node{value: value, next: cur}
+			}
+			break
+		}
+	}
+	return
+}
---
+++ ordered_linked_list_test.go
@@ -0,0 +1,98 @@
+package algos_in_golang
+
+import "testing"
+
+func TestOrderedAscLinkedList_PrintList(t *testing.T) {
+	list := OrderedAscLinkedList{}
+	resStr := list.PrintList()
+	if resStr != "OAscLL: " {
+		t.Error("Expected 'OAscLL ' got", resStr)
+	}
+
+	list.Append(0)
+	resStr = list.PrintList()
+	if resStr != "OAscLL: 0" {
+		t.Error("Expected 'OAscLL 0' got", resStr)
+	}
+
+	vars := struct {
+		values  []int
+		results []string
+	}{
+		values:  []int{1, 2, 3, 4, 5},
+		results: []string{"OAscLL: 0,1", "OAscLL: 0,1,2", "OAscLL: 0,1,2,3", "OAscLL: 0,1,2,3,4", "OAscLL: 0,1,2,3,4,5"},
+	}
+
+	for i := 0; i < len(vars.values); i++ {
+		list.Append(vars.values[i])
+		resStr = list.PrintList()
+		if resStr != vars.results[i] {
+			t.Error("Expected ", vars.results[i], "got", resStr)
+		}
+	}
+}
+
+func TestOrderedAscLinkedList_Append(t *testing.T) {
+	vars := struct {
+		values  [][]int
+		results []string
+	}{
+		values: [][]int{
+			{1, 10},
+			{10, 1},
+			{10, 2, 3},
+			{2, 10, 3},
+			{2, 3, 10},
+			{10, 3, 2},
+			{3, 2, 10},
+			{3, 10, 2},
+			{1, 2, 3, 4},
+			{2, 1, 3, 4},
+			{2, 3, 1, 4},
+			{2, 3, 4, 1},
+			{2, 1, 3, 4},
+			{2, 3, 1, 4},
+			{2, 3, 4, 1},
+			{3, 1, 2, 4},
+			{1, 3, 2, 4},
+			{1, 2, 4, 3},
+			{4, 1, 2, 3},
+			{1, 4, 2, 3},
+			{1, 2, 4, 3},
+		},
+		results: []string{
+			"OAscLL: 1,10",
+			"OAscLL: 1,10",
+			"OAscLL: 2,3,10",
+			"OAscLL: 2,3,10",
+			"OAscLL: 2,3,10",
+			"OAscLL: 2,3,10",
+			"OAscLL: 2,3,10",
+			"OAscLL: 2,3,10",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+			"OAscLL: 1,2,3,4",
+		},
+	}
+
+	for i := 0; i < len(vars.values); i++ {
+		list := OrderedAscLinkedList{}
+		for j := 0; j < len(vars.values[i]); j++ {
+			list.Append(vars.values[i][j])
+		}
+		resStr := list.PrintList()
+		if resStr != vars.results[i] {
+			t.Error("Expected ", vars.results[i], "got", resStr)
+		}
+	}
+}
